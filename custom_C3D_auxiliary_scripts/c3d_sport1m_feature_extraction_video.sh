3d9d1159126394fbd94f4ead87590258e835fe3c
Vivaaindrean Ng <40485262+VivaaindreanNg@users.noreply.github.com>
2019-06-27 04:51:21 +0800

    Update c3d_sport1m_feature_extraction_video.sh

--- custom_C3D_auxiliary_scripts/c3d_sport1m_feature_extraction_video.sh
+++ custom_C3D_auxiliary_scripts/c3d_sport1m_feature_extraction_video.sh
@@ -11,7 +11,7 @@ path_vid=${full_path_vid_file%.*}
 #Get type of dir (immediate parent folder, ie: Abuse, Explosion,...) only
 mkdir -p /home/vivaainng/Desktop/AnomalyDetectionCVPR2018/C3D_extracted_features/${path_vid#./input/Videos/}
 
-GLOG_logtosterr=1 ../../build/tools/extract_image_features.bin prototxt/c3d_sport1m_feature_extractor_video.prototxt conv3d_deepnetA_sport1m_iter_1900000 0 50 20 prototxt/output_list_video_prefix.txt fc6-1
+GLOG_logtosterr=1 ../../build/tools/extract_image_features.bin prototxt/c3d_sport1m_feature_extractor_video.prototxt conv3d_deepnetA_sport1m_iter_1900000 0 50 40 prototxt/output_list_video_prefix.txt fc6-1
 done
 
 
@@ -20,6 +20,6 @@ done
 #mkdir -p <path/to/output>/crimetype/video 
 
 #Changed <num_mini_batches> from 1 to 20
-#GLOG_logtosterr=1 ../../build/tools/extract_image_features.bin prototxt/c3d_sport1m_feature_extractor_video.prototxt conv3d_deepnetA_sport1m_iter_1900000 0 50 20 prototxt/output_list_video_prefix.txt fc6-1
+#GLOG_logtosterr=1 ../../build/tools/extract_image_features.bin prototxt/c3d_sport1m_feature_extractor_video.prototxt conv3d_deepnetA_sport1m_iter_1900000 0 50 40 prototxt/output_list_video_prefix.txt fc6-1
 
 
