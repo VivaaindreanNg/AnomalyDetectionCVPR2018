fabd82749f66cc6ca334fcc77b036209d0de91e5
Vivaaindrean Ng <40485262+VivaaindreanNg@users.noreply.github.com>
2019-06-27 04:54:15 +0800

    Update c3d_sport1m_feature_extraction_video.sh

--- custom_C3D_auxiliary_scripts/c3d_sport1m_feature_extraction_video.sh
+++ custom_C3D_auxiliary_scripts/c3d_sport1m_feature_extraction_video.sh
@@ -19,7 +19,7 @@ done
 #**********************************For single input video**********************************
 #mkdir -p <path/to/output>/crimetype/video 
 
-#Changed <num_mini_batches> from 1 to 20
+#Changed <num_mini_batches> from 1 to 40
 #GLOG_logtosterr=1 ../../build/tools/extract_image_features.bin prototxt/c3d_sport1m_feature_extractor_video.prototxt conv3d_deepnetA_sport1m_iter_1900000 0 50 40 prototxt/output_list_video_prefix.txt fc6-1
 
 
